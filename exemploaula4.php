24d749e22930190c2c49abf5fe0b97468e2804fb
Igor <igor.pessoa@etec.sp.gov.br>
2023-02-11 14:47:01 -0300

    first commit

---
+++ exemploaula4.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+      //declarando variável
+       $var1=1; //inteiro
+       $varX=9.50;//ponto flutuante
+       $varY=9.50;//ponto flutuante
+       $var2 = $varX + $varY; //recebendo resultado da expressão
+       $varSTR="ATRIBUIR STRING";//TEXTO(STRING)
+       $varletra="a";//caracter
+       $varbool=true;//atribui lógica
+
+       echo "exemplo de aplicação em PHP";
+       print "<P>";
+
+       echo "conteúdo de var1: ";
+       echo $var1;
+       print "<P>";
+    
+       echo "conteudo de varX: ";
+       echo $varX;
+       print "<P>"; 
+
+       echo "conteudo de varY: ";
+       echo $varY;
+       print "<p>";
+
+       echo "conteudo de var2: ";
+       echo $var2;
+       print "<p>";
+
+       echo "conteudo de varSTR: ";
+       echo $varSTR;
+       print "<p>";
+
+       echo "conteudo de varletra: ";
+       echo $varletra;
+       print "<p>";
+
+       echo "conteudo de varbool: ";
+       echo $varbool;
+       print "<p>";
+    ?>    
+
+</body>
+</html> 
